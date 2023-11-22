f98615f971f100af252611c644ef0c6fa93b3016
HAMIDO <hamidenajari9@gmail.com>
2023-11-22 10:43:51 -0800

    added frontend UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,62 @@
+import { useEffect, useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
-
-function Login(){
-    return <div>Login</div>
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  useEffect(() => {});
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start setting goals</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              name="email"
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email"
+              onChange={onChange}
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter password"
+              onChange={onChange}
+              value={password}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
-export default Login
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,86 @@
+import { useEffect, useState } from "react";
+import { FaUser } from "react-icons/fa";
 
-
-function Register(){
-    return <div>Register</div>
+function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = formData;
+  useEffect(() => {});
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please xreate an account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              name="name"
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Enter your name"
+              onChange={onChange}
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              name="email"
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email"
+              onChange={onChange}
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter password"
+              onChange={onChange}
+              value={password}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              name="password2"
+              type="password"
+              className="form-control"
+              id="password2"
+              placeholder="Confirm password"
+              onChange={onChange}
+              value={password2}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
-export default Register
+export default Register;
